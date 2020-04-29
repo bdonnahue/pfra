e14cb57d666d38cad80211cadb544ca2168535dd
root <root@localhost.localdomain>
2020-04-29 06:38:05 +0000

    Adding a MockService to assist in testing

--- tests/install_test_utilities.sh
+++ tests/install_test_utilities.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-	# Install the software for the dns server
+set -e
+set -x
+
+
+# Install the software for the dns server (this is the service we are testing with)
 	yum -y install bind bind-utils net-tools
 
-	
+# Install the pacemaker software
+	yum -y install pcs	
+
+# Install the mock service
+#	https://linuxhint.com/systemd_unit_file_service/
+
+	SCRIPT_DIR=$(dirname "$0")
+ 	yes | cp -f ${SCRIPT_DIR}/MockService/MockService.service /usr/lib/systemd/system/
+	systemctl daemon-reload
